2278c3c8fd96d449ac54fe2c5b46ef6db6f4fee1
lim7635 <ju7635@naver.com>
2023-12-14 21:45:33 +0900

    2023.12.14 자료형 변환

--- Class3th/Source.c
+++ Class3th/Source.c
@@ -69,6 +69,89 @@ void main()
 
 #pragma endregion
 
+#pragma region 중첩 for문
+
+	/*for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("안쪽에 있는 반복문\n");
+		}
+
+		printf("바깥에 있는 반복문\n");
+	}*/
+
+
+#pragma endregion
+
+#pragma region 구구단
+
+	// 1 ~ 9단
+
+	// 1 x 1 = 1
+	// 2 x 3 = 6
+
+	/*for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= 9; j++)
+		{
+			printf("%d x %d = %d\n", i, j, i * j);
+		}
+
+		printf("\n");
+	}*/
+
+#pragma endregion
+
+#pragma region while문
+	// 특정 조건을 만족할 때까지 계속해서 주어진 명령문을 실행하는 반복문입니다.
+
+	/*int count = 5;
+
+	while (count > 0)
+	{
+		printf("Excution\n");
+		count--;
+	}*/
+
+	// while문의 경우 위에서 아래로 실행되며,
+	// 아래에 있는 명령문의 실행이 다 끝나면 다시 위에 있는
+	// 명령문으로 돌아가서 반복하는 구조입니다.
+
+#pragma endregion
+
+#pragma region do-while문
+	// 조건과 상관없이 한 번의 작업을 수행한 다음
+	// 조건에 따라 명령문을 실행하는 반복문입니다.
+
+	/*int count = 0;
+
+	do
+	{
+		printf("Login\n");
+	}
+	while (count > 3);*/
+
+#pragma endregion
+
+#pragma region continue문
+	// 해당 조건문만 실행되지 않고, 반복문은 이어서 실행하는 제어문입니다.
+	
+	/*for (int i = 0; i < 5; i++)
+	{
+		if (i == 3)
+		{
+			continue;
+		}
+
+		printf("i의 값 : %d\n", i);
+
+	}*/
+
+	// 반복문에서 continue문을 만나게 되면
+	// 다시 위에서부터 차례대로 동작합니다.
+
+#pragma endregion
 
 #pragma endregion
 
---
+++ Class4th/Source.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void main()
+{
+#pragma region 자료형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때
+	// 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정입니다.
+
+#pragma region 암묵적 형 변환
+	// 서로 다른 자료형으로 연산이 이루어질 때
+	// 자료형의 크기가 큰 자료형으로 변환되는 과정입니다.
+
+	//int integer = 10;
+	//float decimal = 6.5f;
+
+	//printf("integer(정수형) + decimal(실수형) : %f\n", integer + decimal);
+
+	//// 표현 범위가 작은 데이터에 표현 범위가 큰 데이터를 저정하게 되면
+	//// 암묵적으로 데이터 손실이 발생하게 됩니다.
+
+	//integer = decimal;
+
+	//printf("integer 변수의 값 : %d\n", integer);
+
+#pragma endregion
+
+#pragma region 명시적 형 변환
+	// 연산이 이루어지기 전에 사용자가 직접 자료형을 변환하는 과정입니다.
+
+	/*int x = 5;
+	int y = 2;
+
+	float result = (float)x / y;
+
+	printf("result 변수의 값 : %f\n", result);*/
+
+	// 정수형 변수끼리 연산을 수행하게 되면 정수의 결과값만 가질 수 있습니다.
+
+#pragma endregion
+
+
+#pragma endregion
+
+}

f16d3ded5a0c6fef1cf2caad5b2b55e25c7e1171
lim7635 <ju7635@naver.com>
2023-12-19 19:52:50 +0900

    백준 문제 연습(A+B 출력)

--- Program/Solution.c
+++ Program/Solution.c
@@ -2,5 +2,20 @@
 
 void main()
 {
+	int A = 0;
+	int B = 0;
+	
+	printf("A : ");
+	scanf_s("%d", &A);
+	printf("B : ");
+	scanf_s("%d", &B);
 
+	if (A > 0 && B < 10)
+	{
+		printf("A+B = %d\n", A + B);
+	}
+	else
+	{
+		printf("입력값 오류");
+	}
 }
